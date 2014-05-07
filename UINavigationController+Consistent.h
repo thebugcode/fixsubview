5ae7c0a1e80261d18cabb63dce24fa9197f0118a
Dan Ursu <dan.ursu@yopeso.com>
2014-05-07 13:33:28 +0300

    Added the category

---
+++ UINavigationController+Consistent.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Consistent.h
+//  Home
+//
+//  Created by MacUser on 4/30/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (Consistent) <UINavigationControllerDelegate>
+
+@end
